d93b2a508efda78742328c2c3e44d8b39e8ec1b4
Vlad Eliseev <freshymanchan@gmail.com>
2019-07-17 18:41:43 +1000

    App component refactored

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import NavBar from './Controls/NavBar';
 import Sidebar from './Controls/Sidebar';
 import ContentView from './Controls/ContentView';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  }
-}));
-
-
 function App() {
-	const classes = useStyles();
 
 	return (
-		<div className={classes.root}>
+		<div className="root">
 
 			<NavBar />
 			<Sidebar />
